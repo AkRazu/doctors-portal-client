6dfc4198d29ec22d661d8abab8d7a3c8cd2741b7
Ak Razu <fbook3603@gmail.com>
2023-02-19 06:30:23 +0600

    footer section add

--- src/Pages/Home/Shared/Footer/Footer.js
+++ src/Pages/Home/Shared/Footer/Footer.js
@@ -1,11 +1,63 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
 const Footer = () => {
-    return (
+  const date = new Date();
+
+  const year = date.getFullYear();
+  return (
+    <section className="footer-bg">
+      <footer className="footer p-10 text-neutral-content text-black">
         <div>
-            <h2>This is Footer</h2>
+          <span className="footer-title">Services</span>
+          <Link to="/" className="link link-hover">
+            Branding
+          </Link>
+          <Link to="/" className="link link-hover">
+            Design
+          </Link>
+          <Link to="/" className="link link-hover">
+            Marketing
+          </Link>
+          <Link to="/" className="link link-hover">
+            Advertisement
+          </Link>
         </div>
-    );
+        <div>
+          <span className="footer-title">Company</span>
+          <Link to="/" className="link link-hover">
+            About us
+          </Link>
+          <Link to="/" className="link link-hover">
+            Contact
+          </Link>
+          <Link to="/" className="link link-hover">
+            Jobs
+          </Link>
+          <Link to="/" className="link link-hover">
+            Press kit
+          </Link>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <Link to="/" className="link link-hover">
+            Terms of use
+          </Link>
+          <Link to="/" className="link link-hover">
+            Privacy policy
+          </Link>
+          <Link to="/" className="link link-hover">
+            Cookie policy
+          </Link>
+        </div>
+      </footer>
+      <div>
+        <p className="text-center">
+          Copyright &copy; {year} - All right reserved by Doctors Portal
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
