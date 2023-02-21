7e4e74ed7bb271600f69786769c1ef6f89f54a96
Ak Razu <fbook3603@gmail.com>
2023-02-22 03:29:44 +0600

    Login section add

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,40 +1,78 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
-  const [data, setData] = useState("");
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
+  const handelLogin = (data) => {
+    console.log(data);
+  };
+  console.log();
   return (
     <div className="h-[800px] flex justify-center items-center">
-      <div>
-        <h2 className="text-4xl">Login</h2>
-        <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+      <div className="w-96 p-7 shadow-xl rounded-lg">
+        <h2 className="text-xl">Login</h2>
+        <form onSubmit={handleSubmit(handelLogin)}>
           <div className="form-control w-full max-w-xs">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
             <input
-              {...register("email")}
+              {...register("email", { required: "Email Address is required" })}
               className="input input-bordered w-full max-w-xs "
             />
           </div>
+          {errors.email && (
+            <p className="text-red-600 pt-2" role="alert">
+              {errors.email?.message}
+            </p>
+          )}
           <div className="form-control w-full max-w-xs">
             <label className="label">
               <span className="label-text">Password</span>
             </label>
             <input
-              {...register("password")}
-              type='password'
+              {...register("password", {
+                required: "Password is required",
+                minLength: { value: 6, message: "Password must be 6 characters or longer" },
+              })}
+              type="password"
               className="input input-bordered w-full max-w-xs"
             />
+            {errors.password && (
+              <p className="text-red-600 pt-2" role="alert">
+                {errors.password?.message}
+              </p>
+            )}
             <label className="label">
-              <span className="label-text-alt">Forgot Password ?</span>
+              <Link to="/forget" className="label-text-alt">
+                Forgot Password ?
+              </Link>
             </label>
           </div>
 
-          <input className="btn  w-full max-w-xs my-2" type="submit" />
+          <input
+            value="LOGIN"
+            className="btn btn-accent  w-full max-w-xs my-2"
+            type="submit"
+          />
         </form>
+        <p className="text-center my-4">
+          New to Doctors Portal?
+          <Link className="text-secondary" to="/signup">
+            Create new account
+          </Link>
+        </p>
+
+        <div className="divider">OR</div>
+        <button className="btn btn-outline w-full max-w-xs">
+          CONTINUE WITH GOOGLE
+        </button>
       </div>
     </div>
   );
